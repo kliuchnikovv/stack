3c7f0fad5cef258b6560fb8508f79959d26ee855
Valeriy <valeriy.v.klyuchnikov@gmail.com>
2020-03-11 16:02:02 +0300

    Benchmarks update

--- stack_test.go
+++ stack_test.go
@@ -2,19 +2,21 @@ package stack
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
+const numberOfIterations = 1000000
+
 func TestStackPop(t *testing.T) {
 	stack := new(Stack)
-	numberOfItems := 10
 
-	for i := 0; i < numberOfItems; i++ {
+	for i := 0; i < numberOfIterations; i++ {
 		stack.Push(i)
 	}
-	assert.Equal(t, numberOfItems, stack.Len())
-	for i, item := range stack.PopN(numberOfItems) {
-		assert.Equal(t, (numberOfItems-1)-i, item.(int))
+	assert.Equal(t, numberOfIterations, stack.Len())
+	for i, item := range stack.PopN(numberOfIterations) {
+		assert.Equal(t, (numberOfIterations-1)-i, item.(int))
 	}
 	v, ok := stack.Pop()
 	assert.Nil(t, v)
@@ -25,8 +27,8 @@ func TestStackPop(t *testing.T) {
 func BenchmarkStack_Push(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		stack := Stack{}
-		for i := 0; i < 1000000; i++ {
-			stack.Push(i)
+		for j := 0; j < numberOfIterations; j++ {
+			stack.Push(j)
 		}
 	}
 }
@@ -34,14 +36,27 @@ func BenchmarkStack_Push(b *testing.B) {
 func BenchmarkStack_Pop(b *testing.B) {
 	b.StopTimer()
 	stack := Stack{}
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < numberOfIterations; i++ {
 		stack.Push(i)
 	}
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		st := stack
-		for i := 1000000; i > 0; i-- {
+		for j := numberOfIterations; j > 0; j-- {
 			st.Pop()
+		}
+	}
+}
+
+func BenchmarkPushPopRandomly(b *testing.B) {
+	stack := Stack{}
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < numberOfIterations; j++ {
+			if stack.Len() == 0 || rand.Int() == 0 {
+				stack.Push(j)
+			} else {
+				stack.Pop()
+			}
 		}
 	}
 }
